3fddf042bc754e22dd7ad321b7072e8127c8063f
Amirhossein Yaghoubi <amir.yaghoubi.dev@gmail.com>
2023-05-16 08:10:49 +0330

    using local state for loading data instead of IO

--- savepoint/src/lib.rs
+++ savepoint/src/lib.rs
@@ -1,15 +1,25 @@
+use std::sync::{Arc, Mutex};
+
 use tokio::fs;
 pub struct SavePointService {
     storage_path: String,
+    check_point: Arc<Mutex<u32>>,
 }
 
 impl SavePointService {
     pub fn new(storage_path: String) -> Self {
-        SavePointService { storage_path }
+        SavePointService {
+            storage_path,
+            check_point: Arc::new(Mutex::new(0)),
+        }
     }
 
     pub async fn save(&self, id: u32) -> Result<(), String> {
         println!("saving id: {} into {}", id, self.storage_path);
+
+        let mut check_point = self.check_point.lock().unwrap();
+        *check_point = id;
+
         fs::write(self.storage_path.as_str(), id.to_string())
             .await
             .unwrap();
@@ -17,10 +27,21 @@ impl SavePointService {
     }
 
     pub async fn load(&self) -> Result<u32, String> {
+        let check_point = self.check_point.lock().unwrap().clone();
+
+        if check_point > 0 {
+            return Ok(check_point);
+        }
+
         let result = fs::read_to_string(self.storage_path.as_str()).await;
 
         match result {
-            Ok(content) => Ok(content.trim().parse().unwrap()),
+            Ok(content) => {
+                let id = content.trim().parse().unwrap();
+                let mut check_point = self.check_point.lock().unwrap();
+                *check_point = id;
+                Ok(id)
+            }
             Err(err) => {
                 println!("error: {:?}", err);
                 Ok(0)
